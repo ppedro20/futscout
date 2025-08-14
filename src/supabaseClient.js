f89e0f8a39ed6e009228c535be21c1fe5f260f68
Pedro Pedro <pedro.afonso.pedro.pp@gmail.com>
2025-08-14 23:06:17 +0100

    .env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://qcymkjkmlimfhaludlra.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjeW1ramttbGltZmhhbHVkbHJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxODY4NDQsImV4cCI6MjA3MDc2Mjg0NH0.MuS8xJ9yKCSEc3dSyJEiNRQbRd98MGXLx2VDd91MLAo";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
